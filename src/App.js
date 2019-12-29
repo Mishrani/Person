13fe5e35c2b7b1f41428fc29743eb317b333ab48
Mishrani <mishra.nitesh987@gmail.com>
2019-12-29 23:21:33 +0530

    checking id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './Components/Person/Person';
+import Person from './Components/Person/Person';;
 
 class App extends Component{
 
